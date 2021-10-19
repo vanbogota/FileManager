6c686e374ece8ac8fc1f786c6253ce71c4e86daf
Ivan-Dell <vanbog@mail.ru>
2021-10-19 20:33:16 +0300

    done homework of lesson 3

---
+++ Lesson-2/Lesson-3/WorkWithStringsInFile/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace WorkWithStringsInFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //заполнения исходного файла информацией
+            string inputFile = "FileWithFIOandMail.txt";
+                       
+            List<string> stringsWithFIOandMail = new List<string>();
+            stringsWithFIOandMail.Add("Кучма Андрей Витальевич & Kuchma@mail.ru");
+            stringsWithFIOandMail.Add("Мизинцев Павел Николаевич & Pasha@mail.ru");
+            
+            if (!File.Exists(inputFile))
+            {
+                File.WriteAllLines(inputFile, stringsWithFIOandMail);
+            }
+            //выполнение по заданию
+            else
+            {
+                string mailsList = "FileWithMails.txt";
+
+                List<string> stringsWithMail = new List<string>();
+
+                string[] stringsFromFile = File.ReadAllLines(inputFile);
+
+                for(int i =0; i< stringsFromFile.Length; i++ )
+                {
+                    SearchMail(ref stringsFromFile[i]);
+
+                    stringsWithMail.Add(stringsFromFile[i]);
+                }
+
+                File.AppendAllLines(mailsList, stringsWithMail);
+            }            
+        }
+
+        public static void SearchMail(ref string s) 
+        {
+            string[] strings = s.Split("&");
+            foreach (string item in strings)
+            {
+                if (item.Contains('@'))
+                {
+                    s = item.Trim(' ');
+                }                    
+            }
+        }
+    }
+}
