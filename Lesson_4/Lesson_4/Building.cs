a212c8329e9a991d420a0b014d20b49cedb99977
Ivan-Dell <vanbog@mail.ru>
2021-10-26 23:55:27 +0300

    homework lesson 4 parts 1 and 2*

---
+++ Lesson_4/Lesson_4/Building.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson_4
+{
+    public class Building
+    {        
+        private int id;
+        private int height;
+        private int floors;
+        private int flats;
+        private int entrancies;
+        private static int lastId;
+
+        public Building()
+        {
+            CreateId();
+        }
+        
+        public int GetId()
+        {
+            return id;
+        }
+
+        public int Height
+        {
+            get
+            {
+                return height;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    height = value;
+                }
+            }
+        }
+
+        public int Floors
+        {
+            get
+            {
+                return floors;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    floors = value;
+                }
+            }
+        }
+        public int Flats
+        {
+            get
+            {
+                return flats;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    flats = value;
+                }
+            }
+        }
+        public int Entrancies
+        {
+            get
+            {
+                return entrancies;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    entrancies = value;
+                }
+            }
+        }
+        public float GetHeightOfFloor()
+        {
+            if(floors > 0)
+            {
+                return height / floors;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+        public int GetNumberOfFlatsPerEntrancies()
+        {
+            if(entrancies >0)
+            {
+                return flats / entrancies;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public int GetNumberOfFlatsPerFloor()
+        {
+            if (floors > 0)
+            {
+                return flats / floors;
+            }
+            else
+            {
+                return -1;
+            }             
+        }
+
+        private void CreateId()
+        {
+            id = lastId + 1;
+            lastId = id;            
+        }
+    }
+}
---
+++ Lesson_4/Lesson_4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Lesson_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //test Creator
+           var building0 = Creator.CreateBuild(10,3,0,10);
+           var building1 = Creator.CreateBuild(15, 3, 5, 300);
+
+            Console.WriteLine(
+                $"Number {building0.GetId()}\n" +
+                $"Heigh {building0.Height}\n" +
+                $"Entrance {building0.Entrancies}\n" +
+                $"Floors {building0.Floors}\n" +
+                $"Flats {building0.Flats}\n" +
+                $"FloorHeight {building0.GetHeightOfFloor()}\n" +
+                $"Flats per Enter {building0.GetNumberOfFlatsPerEntrancies()}\n" +
+                $"Flats per floor {building0.GetNumberOfFlatsPerFloor()}");
+            Console.WriteLine();
+            
+            Console.WriteLine(
+                $"Number {building1.GetId()}\n" +
+                $"Heigh {building1.Height}\n" +
+                $"Entrance {building1.Entrancies}\n" +
+                $"Floors {building1.Floors}\n" +
+                $"Flats {building1.Flats}\n" +
+                $"FloorHeight {building1.GetHeightOfFloor()}\n" +
+                $"Flats per Enter {building1.GetNumberOfFlatsPerEntrancies()}\n" +
+                $"Flats per floor {building1.GetNumberOfFlatsPerFloor()}");
+        }
+    }
+}
