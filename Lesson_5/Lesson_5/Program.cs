7cc088aa572c40e89dcc672c7a01cb6f12539ed1
Ivan-Dell <vanbog@mail.ru>
2021-11-02 22:41:12 +0300

    homework of lesson 5 part 1

---
+++ Lesson_5/Lesson_5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lesson_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RationalNumber num1 = new RationalNumber(5,3);
+            var num2 = new RationalNumber(5,3);
+            var num3 = num2;
+
+
+            Console.WriteLine(num3.Equals(num2));
+            Console.WriteLine(num2.Equals(num1));
+
+        }
+    }
+}
