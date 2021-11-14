c9085a0636d799dda794972e677148add11329c8
Ivan-Dell <vanbog@mail.ru>
2021-11-14 14:39:28 +0300

    honework of lesson 5 part 2

--- Lesson_5/Lesson_5/Program.cs
+++ Lesson_5/Lesson_5/Program.cs
@@ -6,13 +6,23 @@ namespace Lesson_5
     {
         static void Main(string[] args)
         {
-            RationalNumber num1 = new RationalNumber(5,3);
+            //rational number test
+            RationalNumber num1 = new RationalNumber(7,7);
             var num2 = new RationalNumber(5,3);
             var num3 = num2;
 
-
             Console.WriteLine(num3.Equals(num2));
             Console.WriteLine(num2.Equals(num1));
+            Console.WriteLine(num3.ToString());
+
+            //complex number test
+            ComplexNumber num4 = new ComplexNumber(4, 5);
+            var num5 = new ComplexNumber(5, 3);
+            var num6 = num5;
+
+            Console.WriteLine(num6.Equals(num5));
+            Console.WriteLine(num5.Equals(num4));
+            Console.WriteLine(num6.ToString());
 
         }
     }
