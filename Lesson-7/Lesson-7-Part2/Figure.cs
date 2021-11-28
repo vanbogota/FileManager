cffe0ff9ad1b5549fbb2e3086e49fe87de8afd4c
Ivan-Dell <vanbog@mail.ru>
2021-11-28 19:39:48 +0300

    homewrork lesson 7 part 2

---
+++ Lesson-7/Lesson-7-Part2/Figure.cs
@@ -0,0 +1,72 @@
+﻿
+namespace Lesson_7_Part2
+{
+    abstract class Figure
+    {
+        public int X;
+        public int Y;
+        bool _visible;
+        Color _color;
+        
+        public Figure(Color color = Color.Black,
+                      bool visible = true,
+                      int x = 0,
+                      int y = 0)
+        {
+            Color = color;
+            Visible = visible;
+            X = x;
+            Y = y;
+        }
+        public bool Visible
+        {
+            get
+            {
+                return _visible;
+            }
+            set
+            {
+                _visible = value;
+            }
+        }
+
+        public Color Color
+        {
+            get
+            {
+                return _color;
+            }
+            set
+            {
+                _color = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Color: {Color};\n" +
+                $"Visible: {Visible};\n" +
+                $"X = {X}; Y = {Y}";
+        }
+
+        public void ChangeColor(Color color)
+        {
+            Color = color;
+        }
+
+        public bool IsVisible(Figure figure)
+        {
+            return figure.Visible;
+        }
+
+        virtual public void MoveGorizontal(int deltaX)
+        {
+            X += deltaX;
+        }
+        virtual public void MoveVertical(int deltaY)
+        {
+            Y += deltaY;
+        }
+        abstract public double Area();
+    }
+}
---
+++ Lesson-7/Lesson-7-Part2/Rectangle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Lesson_7_Part2
+{
+    class Rectangle : Figure
+    {
+        private int _length;
+        private int _width;
+        public Rectangle(Color color = Color.Black, bool visible = true, int x = 0, int y = 0, int length=1, int width=1) : base(color, visible, x, y)
+        {
+            Length = length;
+            Width = width;
+        }
+
+        public int Length
+        {
+            get
+            {
+                return _length;
+            }
+            set
+            {
+                _length = value;
+            }
+        }
+        public int Width
+        {
+            get
+            {
+                return _width;
+            }
+            set
+            {
+                _width = value;
+            }
+        }
+
+        public override double Area()
+        {
+            return Length * Width;
+        }
+    }
+}
