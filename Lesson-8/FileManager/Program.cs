a15ab17393c1736549a34838d99cfa6212b1aa36
Ivan-Dell <vanbog@mail.ru>
2022-01-23 13:48:26 +0300

    FileManager done (lesson 8)

---
+++ Lesson-8/FileManager/Program.cs
@@ -0,0 +1,357 @@
+﻿using FileManagerLibrary;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FileManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {          
+            
+            Files files = new Files();
+
+            Folder folder = new Folder();
+
+            ConsoleColor color = Console.ForegroundColor;
+
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine("FILEMANAGER\n");
+            
+            Console.ForegroundColor = color;
+            
+            Help();
+                        
+            string currentDirectory = Directory.GetCurrentDirectory();
+
+            while (true)
+            {
+
+                //ConsoleKeyInfo key = Console.ReadKey();
+
+                //if (key.Key == ConsoleKey.F1)
+                //{
+                //    Help();
+                //}
+
+                try
+                {                    
+                    Console.WriteLine($"~You are in:\n~{currentDirectory}:");
+
+                    string input = Console.ReadLine();
+
+                    if (input.ToLowerInvariant() == "q")
+                    {
+                        break;
+                    }
+
+                    string[] inputArray = input.Split(" ");
+                                        
+                    switch (inputArray[0].ToLowerInvariant())
+                    {
+                        case "ls":
+                            LS(inputArray, currentDirectory);
+                            break;
+                        case "cr":                            
+                            Create(inputArray, currentDirectory);
+                            break;
+                        case "rm":
+                            Remove(inputArray, currentDirectory);
+                            break;
+                        case "rn":
+                            Rename(inputArray, currentDirectory);
+                            break;
+                        case "cp":
+                            Copy(inputArray, currentDirectory);
+                            break;
+                        case "mv":
+                            Move(inputArray, currentDirectory);
+                            break;
+                        case "size":
+                            Size(inputArray, currentDirectory);
+                            break;
+                        case "find":
+                            Find(inputArray, currentDirectory);
+                            break;
+                        case "info":
+                            Info(inputArray, currentDirectory);
+                            break;
+                        case "cd":
+                            currentDirectory = CD(inputArray, currentDirectory);                            
+                            break;
+                        default:
+                            Console.WriteLine("Anknown command. See help.");
+                            break;
+                    }                    
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }                                
+            }
+        }
+
+        static void Help()
+        {
+            ConsoleColor color = Console.ForegroundColor;
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine(@"HELP
+            Main Commads:
+            ls - показать содержимое текущей папки или диска;
+            cd <path/name> - переход в указанную директорую или папку в текущей директории;
+            cr <name> - создать папку или файл в текущей директории;
+            rm <name> - удалить папку или файл в текущей директории;
+            rn <name of folder or file> <new name> - переименовать папку или файл в текущей директории;
+            cp <name of folder or file> <to path> - копировать папку или файл из текущей директории в указанную директорию;
+            mv <name of folder or file> <to path> - переместить папку или файл;            
+            size <name> - информация о размере папки или файла в байтах;
+            find <name> - поиск по маске;
+            info <name> - статистические данные о текстовом файле (поддерживается только формат .txt);
+            q - выход из программы.           
+         Имена файлов должны быть указаны с расширением.
+            ");
+
+            Console.ForegroundColor = color;
+        }
+
+        //вывод содержимого папки или диска
+        static void LS(string[] userInput, string currentDirectory)
+        {
+            if (userInput.Length > 1)
+            {
+                throw new Exception("Incorrect command. See help.");
+            }
+
+            Folder folder = new Folder();
+
+            folder.DirectoryEntire(currentDirectory);
+
+        }
+        //переход в дргую папку
+        static string CD(string[] userInput, string currentDirectory)
+        {
+            if (userInput.Length == 1)
+            {
+                throw new Exception("Enter folder or file name.");
+            }
+
+            currentDirectory = Path.Combine(currentDirectory, userInput[1]);
+
+            if (!Directory.Exists(currentDirectory))
+            {
+                throw new Exception("Такой папки не существует.");
+            };
+            return currentDirectory;
+        }
+
+        //создание папки или файла
+        static void Create(string[] userInput, string currentDirectory)
+        {
+            if (userInput.Length==1)
+            {
+                throw new Exception("Enter folder or file name.");
+            }
+
+            if (userInput.Length>2)
+            {
+                throw new Exception("Incorrect path.");
+            }
+
+            string pathToCreate = Path.Combine(currentDirectory, userInput[1]);
+
+            if (Path.HasExtension(pathToCreate))
+            {
+                Files files = new Files();
+
+                files.Create(pathToCreate);
+            }
+            else
+            {
+                Folder folder = new Folder();
+
+                folder.Create(pathToCreate);
+            }
+        }
+        //удаление папки или файла
+        static void Remove(string[] userInput, string currentDirectory)
+        {
+            if (userInput.Length == 1)
+            {
+                throw new Exception("Enter folder or file name.");
+            }
+
+            if (userInput.Length > 2)
+            {
+                throw new Exception("Incorrect path.");
+            }
+
+            string pathToRemove = Path.Combine(currentDirectory, userInput[1]);
+
+            if (Path.HasExtension(pathToRemove))
+            {
+                Files files = new Files();
+                files.Delete(pathToRemove);
+            }
+            else
+            {
+                Folder folder = new Folder();
+                folder.Delete(pathToRemove);
+            }
+        }
+        //переименование папки или файла
+        static void Rename(string[] userInput, string currentDirectory)
+        {
+            if (userInput.Length == 1)
+            {
+                throw new Exception("Enter folder or file name.");
+            }
+
+            if (userInput.Length > 3)
+            {
+                throw new Exception("Incorrect command input. See help.");
+            }
+
+            string pathToRename = Path.Combine(currentDirectory, userInput[1]);
+
+            if (Path.HasExtension(pathToRename))
+            {
+                Files files = new Files();
+                files.Rename(pathToRename, userInput[2]);
+            }
+            else
+            {
+                Folder folder = new Folder();
+                folder.Rename(pathToRename, userInput[2]);
+            }
+        }
+        //копирование папки или файла
+        static void Copy(string[] userInput, string currentDirectory)
+        {
+            if (userInput.Length == 1)
+            {
+                throw new Exception("Enter folder or file name.");
+            }
+
+            if (userInput.Length > 3)
+            {
+                throw new Exception("Incorrect command input. See help.");
+            }
+
+            string pathToCopy = Path.Combine(currentDirectory, userInput[1]);
+
+            if (Path.HasExtension(pathToCopy))
+            {
+                Files files = new Files();
+                files.CopyFromTo(pathToCopy, userInput[2]);
+            }
+            else
+            {
+                Folder folder = new Folder();
+                folder.Rename(pathToCopy, userInput[2]);
+            }
+        }
+        //перемещение папки или файла
+        static void Move(string[] userInput, string currentDirectory)
+        {
+            if (userInput.Length == 1)
+            {
+                throw new Exception("Enter folder or file name.");
+            }
+
+            if (userInput.Length > 3)
+            {
+                throw new Exception("Incorrect command input. See help.");
+            }
+
+            string pathToMove = Path.Combine(currentDirectory, userInput[1]);
+
+            if (Path.HasExtension(pathToMove))
+            {
+                Files files = new Files();
+                files.MoveFromTo(pathToMove, userInput[2]);
+            }
+            else
+            {
+                Folder folder = new Folder();
+                folder.MoveFromTo(pathToMove, userInput[2]);
+            }
+        }
+        //вывод размера папки или файла
+        static void Size(string[] userInput, string currentDirectory)
+        {
+            if (userInput.Length == 1)
+            {
+                throw new Exception("Enter folder or file name.");
+            }
+
+            if (userInput.Length > 2)
+            {
+                throw new Exception("Incorrect path.");
+            }
+
+            string pathToSize = Path.Combine(currentDirectory, userInput[1]);
+
+            if (Path.HasExtension(pathToSize))
+            {
+                Files files = new Files();
+                files.GetSize(pathToSize);
+            }
+            else
+            {
+                Folder folder = new Folder();
+                folder.GetSize(pathToSize);
+            }
+        }
+        //поиск папки или файла в указанной директории
+        static void Find(string[] userInput, string currentDirectory)
+        {
+            if (userInput.Length == 1)
+            {
+                throw new Exception("Enter folder or file name.");
+            }
+
+            if (userInput.Length > 2)
+            {
+                throw new Exception("Incorrect command. See help.");
+            }
+            
+            if (Path.HasExtension(userInput[1]))
+            {
+                Files files = new Files();
+                files.FindWithMask(currentDirectory, userInput[1]);
+            }
+            else
+            {
+                Folder folder = new Folder();
+                folder.FindWithMask(currentDirectory, userInput[1]);
+            }            
+        }
+        //вывод информации по текстовому файлу
+        static void Info(string[] userInput, string currentDirectory)
+        {
+            if (userInput.Length == 1)
+            {
+                throw new Exception("Enter folder or file name.");
+            }
+
+            if (userInput.Length > 2)
+            {
+                throw new Exception("Incorrect command. See help.");
+            }
+            string infoPath = Path.Combine(currentDirectory, userInput[1]);
+
+            TextFile file = new TextFile(infoPath);
+            Console.WriteLine(@$"
+Number of lines: {file.NumberOfLines};
+Number of symbols including whitespace: {file.NumberOfSymbolsWithWhiteSpace};
+Number of symbols exluding whitespace: {file.NumberOfSymbolsWithoutWhiteSpace};
+Number of words: {file.NumberOfWords};
+Number of paragraphs: {file.NumberOfParagraphs};
+"); 
+        }
+    }
+}
---
+++ Lesson-8/FileManagerLibrary/Folder.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.IO;
+
+namespace FileManagerLibrary
+{
+    public class Folder : IItem
+    {
+        /// <summary>
+        /// Копирование первой указанной директории во вторую указанную директорию. Пути надо указывать полностью.
+        /// </summary>
+        /// <param name="fromPath"></param>
+        /// <param name="toPath"></param>
+        public void CopyFromTo(string fromPath, string toPath)
+        {
+            if (Directory.GetParent(toPath).FullName == fromPath)
+            {
+                throw new Exception("Конечняя папка, в которую следует поместить файлы, является дочерней для папки, в котрой они находятся.");
+            }
+
+            DirectoryInfo diFromPath = new DirectoryInfo(fromPath);
+
+            var newDir = Path.Combine(toPath, diFromPath.Name);
+
+            Directory.CreateDirectory(newDir);
+
+            foreach (var file in diFromPath.GetFiles())
+            {
+                file.CopyTo(Path.Combine(newDir, file.Name));
+            }
+
+            foreach (var dir in Directory.GetDirectories(fromPath))
+            {                
+                CopyFromTo(dir, newDir);
+            }
+        }            
+        /// <summary>
+        /// Создание необходимой директории. Путь надо указывать полностью
+        /// </summary>
+        /// <param name="path"></param>
+        public void Create(string path)
+        {
+            if (!Directory.Exists(path))
+            {
+                try
+                {
+                    Directory.CreateDirectory(path);
+                    Console.WriteLine("Folder created");
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }                                    
+            }
+            else
+            {
+                Console.WriteLine("Folder exist");
+            }
+        }
+        /// <summary>
+        /// Удаление указанной директории. Путь надо указывать полностью
+        /// </summary>
+        /// <param name="path"></param>
+        public void Delete(string path)
+        {            
+            if (Directory.Exists(path))
+            {
+                try
+                {
+                    Directory.Delete(path,true);
+                    Console.WriteLine("Folder deleted");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Folder doesn't exist");
+            }
+        }
+        /// <summary>
+        /// Перенос первой указанной директории во вторую указанную директорию. Пути надо указывать полностью.
+        /// </summary>
+        /// <param name="fromPath"></param>
+        /// <param name="toPath"></param>
+        public void MoveFromTo(string fromPath, string toPath)
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(fromPath);
+            if (dirInfo.Exists)
+            {
+                try
+                {
+                    if (Directory.Exists(toPath))
+                    {                        
+                        string[] temp = fromPath.Split('\\');
+                        dirInfo.MoveTo(Path.Combine(toPath, temp[temp.Length - 1]));
+                        Console.WriteLine("Folder moved.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Folder {toPath} doesn't exist");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }                                
+            }
+            else
+            {
+                Console.WriteLine($"Folder {fromPath} doesn't exist");
+            }
+        }
+        /// <summary>
+        /// Переименовывает указанную директорию. Путь начальной директории надо указывать полность.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="newName"></param>
+        public void Rename(string path, string newName)
+        {
+            if (string.IsNullOrWhiteSpace(newName))
+            {
+                throw new ArgumentException("The Name can't be empty");
+            }
+
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+
+            if (dirInfo.Exists)
+            {              
+                try
+                {
+                    string[] regardFromPath = newName.Split('\\');
+                    dirInfo.MoveTo(Path.Combine(dirInfo.Parent.FullName, regardFromPath[regardFromPath.Length - 1]));
+                    
+                    Console.WriteLine("Folder renamed.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Folder doesn't exist");
+            }
+        }
+        /// <summary>
+        /// Получение информации о размере папки. Путь нало указывать полностью.
+        /// </summary>
+        public virtual void GetSize(string path)
+        {
+            Console.WriteLine($"The size of {path} is " + GetSizeOfFolder(path) + " bytes");
+        }
+        /// <summary>
+        /// Вывод списка папок и файлов в указанной директории (без рекурсии).
+        /// </summary>
+        /// <param name="path"></param>
+        public void DirectoryEntire(string path)
+        {
+            string[] dirs = null;
+            string[] files = null;
+
+            if (Directory.Exists(path))
+            {
+                try
+                {
+                    dirs = Directory.GetDirectories(path);
+                    files = Directory.GetFiles(path);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);                    
+                }
+
+                if (dirs != null)
+                {
+                    foreach (string directory in dirs)
+                    {
+                        DirectoryInfo nameOfDirectory = new DirectoryInfo(directory);
+                        
+                        Console.WriteLine($"\\{nameOfDirectory.Name}");
+                    }
+                    foreach (string fileDir in files)
+                    {
+                        Console.WriteLine(Path.GetFileName(fileDir));
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Directory \"{path}\" doesn't exist");
+            }                
+        }
+        public void FindWithMask(string path, string mask)
+        {
+            foreach (string folder in Directory.GetDirectories(path, mask, SearchOption.AllDirectories))
+            {
+                Console.WriteLine(folder);
+            }
+        }
+        private long GetSizeOfFolder(string path)
+        {
+            long folderSize = 0;
+
+            foreach (string file in Directory.GetFiles(path))
+            {
+                FileInfo f = new FileInfo(file);
+                folderSize += f.Length;
+            }
+            string[] subDirs = Directory.GetDirectories(path);
+
+            foreach (string subDir in subDirs)
+            {
+                folderSize += GetSizeOfFolder(subDir);
+            }
+            return folderSize;
+        }       
+    }
+}
